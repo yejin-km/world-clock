d3cc724c48311ab1c417bbf5f72c11c324f5583f
yejin-km <124483450+yejin-km@users.noreply.github.com>
2023-10-30 00:31:35 -0400

    added select functions

--- javascript/index.js
+++ javascript/index.js
@@ -1,46 +1,76 @@
 function updateTime() {
-  //london
+  // London
   let londonElement = document.querySelector("#london");
-
   if (londonElement) {
     let londonDateElement = londonElement.querySelector(".date");
     let londonTimeElement = londonElement.querySelector(".time");
     let londonTime = moment().tz("Europe/London");
 
-    londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
+    londonDateElement.innerHTML = londonTime.format("MMMM	Do YYYY");
     londonTimeElement.innerHTML = londonTime.format(
       "h:mm:ss [<small>]A[</small>]"
     );
   }
 
-  //paris
+  // Paris
   let parisElement = document.querySelector("#paris");
-
   if (parisElement) {
     let parisDateElement = parisElement.querySelector(".date");
     let parisTimeElement = parisElement.querySelector(".time");
     let parisTime = moment().tz("Europe/Paris");
 
-    parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+    parisDateElement.innerHTML = parisTime.format("MMMM	Do YYYY");
     parisTimeElement.innerHTML = parisTime.format(
       "h:mm:ss [<small>]A[</small>]"
     );
   }
 
-  //new york
+  // New York
   let newYorkElement = document.querySelector("#new-york");
-
   if (newYorkElement) {
     let newYorkDateElement = newYorkElement.querySelector(".date");
     let newYorkTimeElement = newYorkElement.querySelector(".time");
     let newYorkTime = moment().tz("America/New_York");
 
-    newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do YYYY");
+    newYorkDateElement.innerHTML = newYorkTime.format("MMMM	Do YYYY");
     newYorkTimeElement.innerHTML = newYorkTime.format(
       "h:mm:ss [<small>]A[</small>]"
     );
   }
 }
 
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  let image = null;
+
+  if (cityTimeZone === "Europe/London") {
+    image = "big-ben";
+  }
+  if (cityTimeZone === "Europe/Paris") {
+    image = "eiffel-tower";
+  }
+  if (cityTimeZone === "America/New_York") {
+    image = "liberty";
+  }
+
+  citiesElement.innerHTML = `
+  <div class="city">
+    <div>
+      <h2>${cityName} <img src="images/${image}.png"/></h2>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+  </div>
+  `;
+}
+
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
